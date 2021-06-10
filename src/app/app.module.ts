f95de94aca1655e083c105ea120bd9d8f982c883
marcosjmarin <marcosjmarin@hotmail.com>
2021-06-09 23:26:00 -0300

    1 file modified, 6 added, 6 deleted

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,12 +4,17 @@ import { FormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
 import { HelloComponent } from './hello.component';
-import { InicioComponent } from './inicio/inicio.component';
-import { ArticulosFamiliasComponent } from './articulos-familias/articulos-familias.component';
+import { InicioComponent } from './components/inicio/inicio.component';
+import { ArticulosFamiliasComponent } from './components/articulos-familias/articulos-familias.component';
 
 @NgModule({
-  imports:      [ BrowserModule, FormsModule ],
-  declarations: [ AppComponent, HelloComponent, InicioComponent, ArticulosFamiliasComponent ],
-  bootstrap:    [ AppComponent ]
+  imports: [BrowserModule, FormsModule],
+  declarations: [
+    AppComponent,
+    HelloComponent,
+    InicioComponent,
+    ArticulosFamiliasComponent
+  ],
+  bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {}
